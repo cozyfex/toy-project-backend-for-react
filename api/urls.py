3341be38a49aa5278ac754c522482d7b28c9bfeb
Felix D.H Kang <cozyfex@gmail.com>
2022-12-14 00:55:03 +0900

    webhook url

--- api/urls.py
+++ api/urls.py
@@ -11,5 +11,6 @@ router.register(r'base-board', views.BaseBoardSet, basename='base-board')
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('dump', views.dump_board, name='dump')
+    path('dump', views.dump_board, name='dump'),
+    path('webhook', views.webhook, name='webhook')
 ]
